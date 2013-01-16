c3f4e60476338a0fc03ac81f345541b4e356b4b7
Bruno Menezes <brunodmenezes@gmail.com>
2013-01-15 23:26:14 -0200

    adicionando perovaz ao controller

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,12 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+    perovaz {
+    	resource url:'js/perovaz.js'	
+    }
+
+    handlebars {
+    	resource url : "js/handlebars-1.0.rc.1.js"
+    }
 }
